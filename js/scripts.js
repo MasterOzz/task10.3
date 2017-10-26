f455309f74b40fda5b03a18c28f6c47fe1b722ca
MasterOzz <32138364+MasterOzz@users.noreply.github.com>
2017-10-26 08:45:29 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
-$(function(){
-var carouselList = $("#carousel ul");
-setTimeout(changeSlide, 3500); 
-setInterval(changeSlide, 3500); 
+$(function() {
+	var carouselList = $('#carousel ul');
+	setInterval(changeSlide, 3500);
 
-function changeSlide() {
-	carouselList.animate({'marginLeft':-400}, 400, moveFirstSlide);
-};
-	
-function moveFirstSlide () {
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-};
+	function changeSlide() {
+		carouselList.animate({'margin-left':-700}, 500, moveFirstSlide)
+
+		function moveFirstSlide(){
+			var firstImage = carouselList.find('li:first');
+			var lastImage = carouselList.find('li:last');
+			$(lastImage).after(firstImage);
+			carouselList.css('margin-left', '0');
+		}
+	}
 });
